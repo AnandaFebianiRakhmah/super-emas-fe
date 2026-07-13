933a56855483274bcb65ca5e90a6fc7f520e7ff2
AnandaFebianiRakhmah <anandafebianirakhmah@gmail.com>
2026-07-14 00:03:38 +0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,12 @@
 // src/components/Footer.js
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { FaBuilding, FaEnvelope } from "react-icons/fa";
 import "./Footer.css";
 
 export default function Footer() {
   return (
     <div className="footer-container" id="contact">
       <Container>
-        {/* Legal Information Section */}
-        <Row className="footer-legal-section">
-          <Col md={6} className="legal-item">
-            <div className="legal-icon">
-              <FaBuilding />
-            </div>
-            <div className="legal-content">
-              <span className="legal-label">NAMA PELAKU USAHA:</span>
-              <span className="legal-value">PT SUPER EMAS INDONESIA</span>
-            </div>
-          </Col>
-          <Col md={6} className="legal-item">
-            <div className="legal-icon">
-              <FaEnvelope />
-            </div>
-            <div className="legal-content">
-              <span className="legal-label">EMAIL:</span>
-              <a href="mailto:superemas24@gmail.com" className="legal-value legal-link">
-                superemas24@gmail.com
-              </a>
-            </div>
-          </Col>
-        </Row>
-
-        {/* Divider */}
-        <div className="footer-divider"></div>
-
         {/* Copyright */}
         <Row>
           <Col className="text-center">
