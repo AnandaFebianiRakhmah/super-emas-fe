f516223940795e703ddcfd147827afeb0399a2e6
apple <apple@apples-MacBook-Pro.local>
2026-08-20 19:32:35 +0700

    auto scroll

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -3,11 +3,21 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    if (!hash) {
+      window.scrollTo(0, 0);
+      return undefined;
+    }
+
+    const frameId = window.requestAnimationFrame(() => {
+      const target = document.getElementById(hash.slice(1));
+      target?.scrollIntoView({ behavior: "smooth", block: "start" });
+    });
+
+    return () => window.cancelAnimationFrame(frameId);
+  }, [pathname, hash]);
 
   return null;
 }
